1c3065e10295e8b52ba72e46af0973cf1596fac1
go1earner <mechetin.artur2011@yandex.com>
2022-06-03 22:36:02 +0300

    feat: add solution 5.2

--- cache.go
+++ cache.go
@@ -1,23 +1,57 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	timeLayout = "2006-1-2 15:4:5"
+)
 
 type Cache struct {
+	storage map[string]map[string]string
 }
 
-func NewCache() Cache {
-	return Cache{}
+func NewCache(storage map[string]map[string]string) Cache {
+	return Cache{
+		storage: storage,
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache *Cache) Get(key string) (string, bool) {
+	deadline := cache.storage[key]["deadline"]
+	if deadline != "" {
+		timeNow, _ := time.Parse(timeLayout, time.Now().Format(timeLayout))
+		deadlineTime, _ := time.Parse(timeLayout, deadline)
+		fmt.Println("The key deadline is", deadlineTime)
+		fmt.Println("Time now is", timeNow)
+		if deadlineTime.Before(timeNow) {
+			delete(cache.storage, key)
+		}
+	}
+	v, ok := cache.storage[key]["value"]
+	return v, ok
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.storage[key] = map[string]string{
+		"value":    value,
+		"deadline": "",
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Keys() []string {
+	keys := make([]string, 0)
+	for key := range cache.storage {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.storage[key] = map[string]string{
+		"value":    value,
+		"deadline": deadline.Format(timeLayout),
+	}
 }
